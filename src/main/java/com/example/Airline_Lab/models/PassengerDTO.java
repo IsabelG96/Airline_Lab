09efd1ebfad7df86fe70c841522631504529fb80
Isabel <isabelgalwey@gmail.com>
2023-04-20 18:52:27 +0100

    service layers and DTO

--- src/main/java/com/example/Airline_Lab/models/PassengerDTO.java
+++ src/main/java/com/example/Airline_Lab/models/PassengerDTO.java
@@ -1,4 +1,41 @@
 package com.example.Airline_Lab.models;
 
+import java.util.List;
+
 public class PassengerDTO {
+
+    private String name;
+    private String contactDetails;
+
+    private List<Long> flightIDs;
+
+    public PassengerDTO(String name, String contactDetails, List flightIDs){
+        this.name = name;
+        this.contactDetails = contactDetails;
+        this.flightIDs = flightIDs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(String contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<Long> getFlightIDs() {
+        return flightIDs;
+    }
+
+    public void setFlightIDs(List<Long> flightIDs) {
+        this.flightIDs = flightIDs;
+    }
 }
--- src/main/java/com/example/Airline_Lab/services/PassengerService.java
+++ src/main/java/com/example/Airline_Lab/services/PassengerService.java
@@ -1,6 +1,8 @@
 package com.example.Airline_Lab.services;
 
 import com.example.Airline_Lab.models.Flight;
+import com.example.Airline_Lab.models.Passenger;
+import com.example.Airline_Lab.models.PassengerDTO;
 import com.example.Airline_Lab.repositories.FlightRepository;
 import com.example.Airline_Lab.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,15 @@ public class PassengerService {
    @Autowired
    FlightRepository flightRepository;
 
-   public PassengerRepository savePassenger()
+   public PassengerRepository savePassenger(PassengerDTO passengerDTO){
+   Passenger passenger = new Passenger(passengerDTO.getName(), passengerDTO.getContactDetails());
+   for (Long flightId : passengerDTO.getFlightIDs()){
+      Flight flight = flightService.findFlight(flightId);
+      passenger.addFlight(flight);
+   }
+   return passengerRepository.save(passenger);
+}
 
+public Passenger findPassenger(Long id) {
+   return flightRepository.findById(id).get();}
 }
